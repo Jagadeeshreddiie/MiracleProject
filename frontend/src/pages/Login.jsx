19b9a7a21e8497c51446475852b1cfdfea3e8376
Jagadeesh Reddy <105237455+Jagadeeshreddiie@users.noreply.github.com>
2024-03-01 12:26:35 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:30021/login", {
+            const response = await fetch("https://miracle-project-backend1.vercel.app/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
